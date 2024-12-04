c860b4d8d7b8f35274f4546d53af0fa29323be9f
cesar04 <cdelgadom004@gmail.com>
2024-12-04 21:42:05 +0100

    Navidad

---
+++ ejerciciosClase/animaciones/Práctica Postal Navideña/EJ14. Navidad/script.js
@@ -0,0 +1,6 @@
+// El archivo js...
+// Así os ahorro el gran trabajo y sudor que supone crearlo y así Miguel está obligado a no integrarlo en el HTML
+function elem_visibles() {
+    var myAni = document.querySelector('.marandina1');
+    myAni.classList.toggle('marandina2')
+}
