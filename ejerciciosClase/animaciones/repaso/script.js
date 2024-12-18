d496bd851f3082314ea67f2f77f4dcea7447d62a
cesaar04 <cdelgadom004@gmail.com>
2024-12-18 12:41:52 +0100

    repaso

--- ejerciciosClase/animaciones/repaso/script.js
+++ ejerciciosClase/animaciones/repaso/script.js
@@ -2,3 +2,43 @@ function telonTranslate(){
     const telonMovido = document.querySelector('.telon');
     telonMovido.classList.toggle('telon2');
 }
+
+function clicarCalcetin() {
+    const original = document.querySelector('.calcetin');
+    original.classList.toggle('calcetin2');
+}
+
+function clicarReno () {
+    const reno = document.querySelector('.reno');
+    reno.classList.toggle('reno2');
+}
+
+function clicarMuneco () {
+    const muneco = document.querySelector('.muneco');
+    muneco.classList.toggle('muneco2');
+}
+
+function clicarCopo () {
+    const copo = document.querySelector('.copo');
+    copo.classList.toggle('copo2');
+}
+
+function clicarBolas() {
+    const bolas = document.querySelector('.bolas');
+    bolas.classList.toggle('bolas2');
+}
+
+function clicarGalleta() {
+    const galleta = document.querySelector('.galleta');
+    galleta.classList.toggle('galleta2');
+}
+
+function clicarRegalos() {
+    const regalos = document.querySelector('.regalos');
+    regalos.classList.toggle('regalos2');
+}
+
+function clicarBaston() {
+    const baston = document.querySelector('.baston');
+    baston.classList.toggle('baston2')
+}
